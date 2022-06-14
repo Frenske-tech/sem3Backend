1475ce690814d0391500564dee7f3f808d7bdeda
Francois Haan <461403@student.fontys.nl>
2022-06-14 20:59:31 +0200

    Readded crossorigin

--- review/src/main/java/com/example/review/controller/ReviewController.java
+++ review/src/main/java/com/example/review/controller/ReviewController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/review")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReviewController {
 
     @Autowired
