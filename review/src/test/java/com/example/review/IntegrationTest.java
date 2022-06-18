e851964f47fee535608acc270baa97c06fe89077
Frenske99 <39583221+Frenske99@users.noreply.github.com>
2022-06-18 09:09:33 +0200

    Fixed content integration test

--- review/src/test/java/com/example/review/IntegrationTest.java
+++ review/src/test/java/com/example/review/IntegrationTest.java
@@ -12,14 +12,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
@@ -28,10 +33,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import org.springframework.test.web.servlet.ResultActions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 @AutoConfigureMockMvc
@@ -55,36 +62,14 @@ public class IntegrationTest{
 
         mvc.perform(get("/review/getall")).andDo(print()).andExpect(status().isOk());
     }
-//    @Test
-//    void CreateReview() throws Exception{
-//        // given - precondition or setup
-//        Review review = new Review(1, "tessting", "test", 3);
-//
-//
-//        // when - action or behaviour that we are going test
-//        ResultActions response = mvc.perform(post("/review/add")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(review)));
-//
-//        // then - verify the result or output using assert statements
-//        response.andDo(print()).
-//                andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.post",
-//                        is(review.getPost())))
-//                .andExpect(jsonPath("$.song",
-//                        is(review.getSong())))
-//                .andExpect(jsonPath("$.username",
-//                        is(post.getUsername())))
-//                .andExpect(jsonPath("$.userimage",
-//                        is(post.getUserimage())));
-//    }
-//    @Test
-//    void create() throws Exception{
-//        Review review = new Review(1, "testing", "test", 3);
-//        mvc.perform(post("/review/add/")
-//                .contentType(MediaType.ALL)
-//                .content(objectMapper.writeValueAsString(review))
-//        ).andExpect(status().isOk());
-//    }
+
+    @Test
+    void create() throws Exception{
+        Review review = new Review(1, "testing", "test", 3);
+        mvc.perform(MockMvcRequestBuilders.post("/review/add/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(review))
+        ).andExpect(status().isOk());
+    }
 
 }
